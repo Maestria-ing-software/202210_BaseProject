7b3136ea215d60c801cea88ce72bf85a4c4d3f79
carlos castillo <carlos.castillo@zinobe.com>
2023-04-19 10:26:55 -0400

    tallerFinalizado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule,HttpClientModule],
+      declarations: [AppComponent, BookListComponent],
     }).compileComponents();
   });
 
@@ -26,7 +28,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
+
+    expect(compiled.querySelector('.content span')?.textContent).not.toContain(
       'base-project app is running!'
     );
   });
